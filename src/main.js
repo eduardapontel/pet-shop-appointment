a39b6b8097cd8c2346137a6152acb84498d2496e
Eduarda Pontel <eduardapontel@gmail.com>
2025-02-24 02:14:24 +0000

    install and configure packages

---
+++ src/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+import './libs/dayjs.js';
+
+import './styles/global.css';
+import './styles/header.css';
+import './styles/main/main.css';
+import './styles/main/list.css';
+import './styles/footer.css';
+
+import dayjs from 'dayjs';
+console.log(dayjs().format('DD/MM HH:mm'));
