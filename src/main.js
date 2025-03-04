5239e3de9370914a57b2c644ee1d2f03d0d1ae20
Eduarda Pontel <eduardapontel@gmail.com>
2025-03-04 03:30:11 +0000

    add and style <modal> element

--- src/main.js
+++ src/main.js
@@ -7,6 +7,14 @@ import './styles/header.css';
 import './styles/main/main.css';
 import './styles/main/list.css';
 import './styles/footer.css';
+import './styles/modal.css';
 
 import dayjs from 'dayjs';
 console.log(dayjs().format('DD/MM HH:mm'));
+
+// import { openingHours } from './utils/opening-hours.js';
+// const inputTime = document.getElementById('input-time');
+// console.log(openingHours[0]);
+
+// inputTime.min = openingHours[0];
+// inputTime.max = openingHours[openingHours.length - 1];
